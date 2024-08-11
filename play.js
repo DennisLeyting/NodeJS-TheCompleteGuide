200ec5e1fdb55ba4e7790effde011cb64953714b
DennisLeyting <dennisleyting@gmail.com>
2024-08-11 13:42:14 +0200

    Update play.js

--- play.js
+++ play.js
@@ -1,16 +1,37 @@
-const fname = "Max";
-let age = 29;
-const hobbies = true;
+// const person = {
+//   name: "Max",
+//   age: 29,
+//   greet() {
+//     console.log("Hi, I am " + this.name);
+//   },
+// };
 
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    ", and the user has hobbies " +
-    userHasHobby
-  );
-};
+// const printName = (personData) => {
+//   console.log(personData.name)
+// }
+// const printName = ({ name }) => {
+//   // Object destructering. We only need name, so dont need personData
+//   console.log(name);
+// };
 
-console.log(summarizeUser(fname, age, hobbies));
+// const { name, age } = person;
+
+// const hobbies = ["Sports", "Cooking"];
+// const hobby1, hobby2 = hobbies
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+// console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+// console.log(hobbies);
+// const copiedArray = hobbies.slice();
+// console.log(copiedArray);
+// const copiedArray2 = [hobbies];
+// console.log(copiedArray2);
+// const copiedArray3 = [...hobbies]; //spread operator, copy one by one
+// console.log(copiedArray3);
+
+// const toArray = (...args) => { //rest operator, to get all arguments, however many were given
+//   return args;
+// };
+// console.log(toArray(1, 2, 3, 4));
